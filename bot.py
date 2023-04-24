f48e353fea2d4b714649e410f18a66bedf6515bb
Petr Oganin <necroshizo@gmail.com>
2023-04-24 21:58:58 +0300

    alfa0.3

--- bot.py
+++ bot.py
@@ -1,23 +1,16 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types, html
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from aiogram.filters import CommandObject, Text
-from aiogram.types.dice import DiceEmoji
 from config_reader import config
 
-# from aiogram import F
 from magic_filter import F
-from datetime import datetime
-from aiogram.utils.markdown import hide_link
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from contextlib import suppress
 from aiogram.exceptions import TelegramBadRequest
-from random import randint
-from math import comb, factorial
+from math import comb
 from typing import Optional
 from aiogram.filters.callback_data import CallbackData
-from dataclasses import dataclass
 
 
 logging.basicConfig(level=logging.INFO)
@@ -42,6 +35,7 @@ class RollCallbackFactory(CallbackData, prefix="roll"):
 
 
 def get_keyboard():
+    """Описание клавиатуры"""
     builder = InlineKeyboardBuilder()
     # dices
     builder.button(text="2", callback_data=RollCallbackFactory(action="dices", value=2))
@@ -94,108 +88,28 @@ def get_keyboard():
     return builder.as_markup()
 
 
+# TODO неверное общее вычесление с критическим успехом проверить
 def get_probility(dices: int, skill: int, motive: int, complexity: int):
-    def choose(N, i):
-        """
-        Вычисляет количество комбинаций из N элементов, выбирая i элементов из них.
-        """
-        from math import factorial
-
-        return factorial(N) // (factorial(i) * factorial(N - i))
-
-    def binom(i, N, T1, T2):
-        p1 = (T1 // 20) + 1
-        q1 = (T1 % 20) / 20
-        p2 = (T2 // 20) + 1
-        q2 = (T2 % 20) / 20
-        prob = (
-            choose(N, i)
-            * ((q1**i) * ((1 - p1) ** (N - i)))
-            * (
-                (q2 ** (i - min(i, (N - i + 1))))
-                * ((1 - p2) ** (N - i - max(0, (N - i + 1) - i)))
-            )
-        )
-        return prob
-
+    """Подсчет вероятностей успеха"""
     success = (skill + motive) / DICE
+    crit_succes = skill / DICE
     fail = 1 - success
+    crit_fail = 1 - crit_succes
     probability = 1 - sum(
-        comb(dices, i) * success**i * fail ** (dices - i) for i in range(complexity)
+        comb(dices, i) * success**i * fail ** (dices - i)
+        for i in range(complexity)
     )
+    crit_probability = 1 - sum(
+        comb(dices, i) * crit_succes**i * crit_fail ** (dices - i)
+        for i in range(complexity)
+    )
+    all_probability = probability * (1 + crit_probability)
 
-    # success_skill = (skill - 1) / DICE
-    # success_motive = motive / DICE
-    # success_critical = success_skill + success_motive
-    # fail_critical = 1 - success_critical
-    # probability_critical = 0
-    # for i in range(complexity, dices + 1):
-    #     probability_critical += (comb(dices, i
-    #                                   ) * ((success_skill**(2 * i - dices)
-    #                                         ) * ((success_critical**(dices - i)
-    #                                               ) * (fail_critical ** i
-    #                                                    ) * (
-    #                                                     min(i, (dices - i + 1))
-    #                                                     + (i > 0 and (dices - i + 1) > 0)
-    #                                                     )))                                                           )
-    # (min(i, (N - i + 1)) + (i > 0 and (N - i + 1) > 0))))
-
-    N = dices
-    T1 = skill
-    T2 = motive
-    S = complexity
-
-    prob = 0
-    for i in range(S, N + 1):
-        prob += (
-            choose(N, i) * (T1 / 20) ** (2 * i - N) * ((T1 + T2) / 20) ** (N - 2 * i)
-        )
-
-    return round(probability * 100, 2), round(prob * 100, 2)
-
-    # p1 = (T1 - 1) / 20
-    # p2 = T2 / 20
-    # q = 1 - (p1 + p2)
-    # prob = 0
-    # for i in range(S, N+1):
-    #     prob += (comb(N, i) *
-    #              ((p1 ** (2 * i - N)) *
-    #               ((p1 + p2) ** (N - i)) *
-    #               (q ** i) *
-    #               (min(i, (N - i + 1)) + (i > 0 and (N - i + 1) > 0))))
-
-    # p1 = (T1 - 1) / 20  # вероятность успеха на одном броске для первого случая
-    # p2 = T2 / 20  # вероятность успеха на одном броске для второго случая
-    # q = 1 - p1 - p2  # вероятность неудачи на одном броске
-
-    # вычисляем вероятность успеха в S или более бросках
-    # prob = sum([choose(N, i) * p1**(2*i ) * (p2+p1)**(-i) * q**(N-3*i) for i in range(S // 2 + 1)])
-
-    # p1 = (T1 - 1) / 20
-    # p2 = T2 / 20
-    # q = 1 - (p1 + p2)
-    # prob = 0
-    # for i in range(S, N+1):
-    #     prob += comb(N, i) * (p1 ** (2 * i - N)) * ((p1 + p2) ** (N - i)) * (q ** (N-i))
-
-    # if T1 == 0:
-    #     # если T1 = 0, используем формулу биномиального распределения
-    #     T = T2
-    #     p = (T // 20) + 1
-    #     q = (T % 20) / 20
-    #     prob = 1 - ((1 - q) ** N) * ((1 - p) ** S)
-    # else:
-    #     # иначе используем формулу, учитывающую оба значения T1 и T2
-    #     prob = 0
-    #     for i in range(S, N + 1):
-    #         prob += binom(i, N, T1, T2)
-
-    # return round(probability * 100, 2), round(probability_critical * 100, 2)
-
-    # return round(probability * 100, 2), round(success_prob * 100, 2)
+    return probability * 100, all_probability * 100
 
 
 async def update_prob_text(message: types.Message, new_pull: dict[str, int]):
+    """Обновление сообщения"""
     with suppress(TelegramBadRequest):
         probility, probability_critical = get_probility(*new_pull.values())
         dice, skill, motiv, complexity = new_pull.values()
@@ -210,6 +124,7 @@ async def update_prob_text(message: types.Message, new_pull: dict[str, int]):
 
 @dp.message(Command("rolldice"))
 async def cmd_numbers(message: types.Message):
+    """Инициализация запроса"""
     user_data[message.from_user.id] = PULL_DEFAULT
     data: dict = user_data[message.from_user.id]
     probility, probability_critical = get_probility(*data.values())
@@ -271,8 +186,6 @@ async def callbacks_stop_change_fab(callback: types.CallbackQuery):
     await callback.answer()
 
 
-#
-# Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
 
